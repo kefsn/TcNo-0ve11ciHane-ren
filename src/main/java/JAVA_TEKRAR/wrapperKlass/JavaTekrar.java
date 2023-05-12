ef5c035ce16cbed8237702a85265f9df32007397
Mrt Nzl <k.efsne@gmail.com>
2023-05-12 11:05:29 +0300

    Team

---
+++ src/main/java/JAVA_TEKRAR/wrapperKlass/JavaTekrar.java
@@ -0,0 +1,30 @@
+package JAVA_TEKRAR.wrapperKlass;
+
+import java.util.Scanner;
+
+public class JavaTekrar {
+
+    public static void main(String[] args) {
+        int sayi1 = 10;
+        int sayi2 = 20;
+        swap(sayi1,sayi2);
+    }
+
+    private static void swap(int sayi1, int sayi2) {
+        int sayi3;
+        sayi1= sayi1+sayi2;
+        sayi2= sayi1-sayi2;
+        sayi1= sayi1-sayi2;
+        System.out.println(sayi1);
+        System.out.println(sayi2);
+//second way;
+        /*
+        sayi3 = sayi1;
+        sayi1 = sayi2;
+        sayi2 = sayi3;
+        */
+
+
+
+    }
+}
--- src/main/java/ssgDersler/Efsane.java
+++ src/main/java/ssgDersler/Efsane.java
@@ -1,7 +1,9 @@
 package ssgDersler;
 
 import javax.xml.namespace.QName;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -14,20 +16,39 @@ public class Efsane {
     public static void main(String[] args) {
 
 
+
+/*
+
+       System.out.println("Hatırla ey yüce insan...");
+
         Scanner scan = new Scanner(System.in);
-        System.out.println("LÜtfen bir sayı giriniz...");
+        System.out.println("Lütfen T.C. kimlik numaranızı 9 haneli olarak giriniz..");
+        int tc9hane = scan.nextInt();
 
-        int sayi = scan.nextInt();
-        carpim(sayi);
+do {
+    int s9 = tc9hane % 10;
+    int s8 = (tc9hane / 10) % 10;
+    int s7 = (tc9hane / 100) % 10;
+    int s6 = (tc9hane / 1000) % 10;
+    int s5 = (tc9hane / 10000) % 10;
+    int s4 = (tc9hane / 100000) % 10;
+    int s3 = (tc9hane / 1000000) % 10;
+    int s2 = (tc9hane / 10000000) % 10;
+    int s1 = (tc9hane / 100000000) % 10;
+    int s0 = (tc9hane / 1000000000) % 10;
+    int toplam = 7*(s9 + s7 + s5 + s3 + s1 + s0);
+    int eksi = (s8 + s6 + s4 + s2);
 
+
+    System.out.println("10. rakam : "+ (toplam-eksi));
+    System.out.println("11. rakam : " +(s9 + s7 + s5 + s3 + s1 + s0+s8 + s6 + s4 + s2) );
+    break;
+
+}while(true);
+
+
+ */
 
 
     }
-
-    private static void carpim(int sayi) {
-        sayi = sayi * 3;
-        System.out.println(sayi);
-
-    }
-
 }
